06e9da9f7738bd02373070787ab852b33e72b2a8
戴震 <daizhen@daizhendeMacBook-Air.local>
2017-08-03 16:31:14 +0800

    created the project

---
+++ src/components/common/header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header class="ui-header ui-header-positive ui-border-b">
+        <div class="ui-avatar-s">
+            <span style="background-image:url(http://placeholder.qiniudn.com/80x80)"></span>
+        </div>
+        <h1>{{message}}</h1>
+        <i class="ui-icon-add pull-right"></i>
+    </header>
+</template>
+
+<script>
+    export default {
+        props:['message'],
+    }
+</script>
+
+<style>
+    header .ui-avatar-s {
+        display:inline-block;
+        float:left;
+
+        width:35px;
+        height:35px;
+        margin-top:4px;
+    }
+    header h1 {
+        position:absolute;
+        top:0;
+        text-align:center;
+        height:45px;
+        width:100%;
+        margin-left:-10px;
+    }
+    .pull-right {
+        float:right;
+    }
+</style>
---
+++ src/components/pages/message.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="message">
+    <q-header message="消息"></q-header>
+    
+</div>
+</template>
+
+<script>
+import qHeader from '../common/header.vue'
+
+export default {
+    components:{
+        qHeader
+    }
+// import qFooter from '../common/footer'
+}
+</script>
+
+<style scoped>
+    
+</style>
